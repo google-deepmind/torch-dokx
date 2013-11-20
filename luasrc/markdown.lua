b23e6273de69ddc21da9adf08b807dd56bcb6400
Dan Horgan <danhgn@gmail.com>
2013-11-20 16:09:21 +0000

    Markdown output: decrease heading size
    Also add docs; remove debug prints

--- luasrc/markdown.lua
+++ luasrc/markdown.lua
@@ -15,7 +15,7 @@ Args:
  * `packageName` - string; name of package being documented
 
 Returns: new MarkdownWriter object
-]]
+--]]
 function MarkdownWriter:_init(outputPath, packageName)
     self.outputFile = io.open(outputPath, 'w')
     self.packageName = packageName
@@ -23,16 +23,41 @@ function MarkdownWriter:_init(outputPath, packageName)
     self:writeHeader()
 end
 
+--[[ Append a string to the output
+
+Args:
+ * `text` :: string; text to append
+
+Returns: nil
+--]]
 function MarkdownWriter:write(text)
     self.outputFile:write(text)
 end
 
+--[[ Add the package header to the output ]]
 function MarkdownWriter:writeHeader()
-    self:write("# Module " .. self.packageName .. "\n\n")
+    self:heading("Module " .. self.packageName)
 end
 
+--[[ Add a heading to the output
+
+Args:
+ * `text` :: string; the heading text
+
+Returns: nil
+--]]
+function MarkdownWriter:heading(text)
+    self:write("### ".. text .. "\n\n")
+end
+
+--[[ Add markdown for a documented function
+
+Args:
+ * `entity` :: DocumentedFunction object
+
+Returns: nil
+--]]
 function MarkdownWriter:documentEntity(entity)
-    print(entity:str())
     logger:debug("Outputting markdown for " .. entity:name())
 
     local valueTable = {
@@ -40,12 +65,32 @@ function MarkdownWriter:documentEntity(entity)
         doc = entity:doc() or "{missing docs}",
     }
 
-    local outputText = "## " .. valueTable.name .. "\n" .. valueTable.doc
+    local outputText = "#### " .. valueTable.name .. "\n" .. valueTable.doc
 
---    local outputText = template:substitute(valueTable)
     self:write(outputText)
 end
 
+--[[ Add markdown for an undocumented function
+
+Args:
+ * `entity` :: Function object
+
+Returns: nil
+--]]
+function MarkdownWriter:undocumentedFunction(entity)
+    logger:debug("Outputting markdown for " .. entity.name)
+
+    local valueTable = {
+        name = entity.name or "{missing name}",
+    }
+
+    local outputText = " * " .. valueTable.name .. "\n"
+
+    self:write(outputText)
+
+end
+
+--[[ Close the writer. _Must be called before exiting_. ]]
 function MarkdownWriter:close()
     io.close(self.outputFile)
 end
