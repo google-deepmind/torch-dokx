a8f7169908526458c5eed050f0be49728d635873
Dan Horgan <danhgn@gmail.com>
2013-11-21 10:06:08 +0000

    Remove unneeded extractor code

--- luasrc/extract2.lua
+++
@@ -1,8 +0,0 @@
-require 'lxsh'
-local inputPath = arg[1]
-
-local content = io.open(inputPath, "rb"):read("*all")
-
-for kind, text, lnum, cnum in lxsh.lexers.lua.gmatch(content) do
-    print(string.format('%s: %q (%i:%i)', kind, text, lnum, cnum))
-end
